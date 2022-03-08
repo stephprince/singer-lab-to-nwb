12ab83db058ed7171096eb8ea9388a91b2030035
Steph Prince <stephanie.m.prince1@gmail.com>
2022-03-08 18:54:48 -0500

    add accurate session start time information

--- singer_lab_to_nwb/singer_lab_nwb_converter.py
+++ singer_lab_to_nwb/singer_lab_nwb_converter.py
@@ -1,7 +1,9 @@
 import warnings
 
-from nwb_conversion_tools import NWBConverter, PhySortingInterface, SpikeGadgetsRecordingInterface
+from nwb_conversion_tools import NWBConverter, PhySortingInterface
+from readTrodesExtractedDataFile3 import readTrodesExtractedDataFile
 from pathlib import Path
+from datetime import datetime, timedelta
 
 from update_task_virmen_interface import UpdateTaskVirmenInterface
 from singer_lab_preprocessing_interface import SingerLabPreprocessingInterface
@@ -33,34 +35,47 @@ class SingerLabNWBConverter(NWBConverter):
     def get_metadata(self):
         metadata = super().get_metadata()
 
+        # add subject info
+        session_id = self.data_interface_objects['VirmenData'].source_data['session_id']
+        virmen_base_path = Path(self.data_interface_objects['VirmenData'].source_data['file_path'])
+        virmen_file_paths = list(virmen_base_path.glob(f'{session_id}*/virmenDataRaw.mat'))
+        session_data = convert_mat_file_to_dict(mat_file_name=virmen_file_paths[0])
+        subject_id = session_data['virmenData']["sessioninfo"]
+        metadata.update(
+            Subject=dict(
+                subject_id=subject_id,
+                species="Mus Musculus",
+                genotype="Wild type, C57BL/6J",
+                sex="Male",
+                age="3-7 months",  # TODO check these ages
+            )
+        )
+
+        # get session start time
+        if self.data_interface_objects['SpikeGadgetsData']:  # if there exists ephys data, use that
+            # load ephys timestamps file
+            raw_ephys_folder = Path(self.data_interface_objects['SpikeGadgetsData'].source_data['raw_data_folder'])
+            timestamps_filename = list(raw_ephys_folder.glob(f'*.raw/*.timestamps.dat'))
+            time_data = readTrodesExtractedDataFile(timestamps_filename[0])  # TODO - get first file from recording
+
+            # extract session start time information
+            file_creation_time = datetime.utcfromtimestamp(int(time_data['system_time_at_creation']) / 1e3)  # convert from ms to s
+            samples_before_start = int(time_data['first_timestamp']) - int(time_data['timestamp_at_creation'])
+            session_start_time = file_creation_time + timedelta(seconds=samples_before_start/int(time_data['clockrate']))
+        else:  # otherwise use behavioral data
+            virmen_df = pd.DataFrame(session_data['virmenData']['data'], columns=session_data['virmenData']['dataHeaders'])
+            virmen_time = virmen_df["time"].apply(lambda x: matlab_time_to_datetime(x))
+            session_start_time = virmen_time[0]
+
         # add general experiment info
-        virmen_file_path = Path(self.data_interface_objects['VirmenData'].source_data['file_path'])
-        session_id = virmen_file_path.parent.stem
-
-
         metadata['NWBFile'].update(
             experimenter=["Steph Prince"],
             session_id=session_id,
             institution="Georgia Tech",
             lab="Singer",
             session_description="Head-fixed mice performed update task in virtual reality",
+            session_start_time=str(session_start_time),
         )
-
-        # add virmen subject info
-        if virmen_file_path:
-            session_data = convert_mat_file_to_dict(mat_file_name=virmen_file_path)
-            subject_id = session_data['virmenData']["sessioninfo"]
-            metadata.update(
-                Subject=dict(
-                    subject_id=subject_id,
-                    species="Mus Musculus",
-                    genotype="Wild type, C57BL/6J",
-                    sex="Male",
-                    age="3-7 months",  # TODO check these ages
-                )
-            )
-        else:
-            warnings.warn(f"Warning: no subject file detected for session {session_id}!")
 
         # add spike sorting column info
         sorted_path = Path(self.data_interface_objects['PhySortingCA1'].source_data['folder_path'])
